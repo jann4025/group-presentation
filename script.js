50f10799ba33799145cea86563ce957ee7bd595a
Jannick Holm <43129680+jann4025@users.noreply.github.com>
2019-11-03 16:54:07 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,14 +4,46 @@ document.querySelector("button").addEventListener("click", removeStart);
 
 function removeStart() {
   document.querySelector("#start-container").classList.add("hide");
-  document.querySelector(".intro").classList.remove("hidden");
+  document.querySelector(".natalie").classList.remove("hidden");
+  document.querySelector("#musik").play();
 }
 
 const animated = document.querySelector(".spotlight-enter-active");
-
 animated.addEventListener("animationend", () => {
-  console.log("Animation ended");
-  document.querySelector(".natalie-name").classList.remove("hidden");
-  document.querySelector(".natalie-name").classList.add("animation-start");
-  animated.classList.add("natalie-name");
+  setTimeout(assignmentNumber, 2000);
 });
+
+function assignmentNumber() {
+ document.querySelector(".natalie").classList.add("hidden");
+  document.querySelector(".info").classList.remove("hidden");
+    setTimeout(showSarah, 3000);
+}
+
+function showSarah() {
+  document.querySelector(".info").classList.add("hidden");
+  document.querySelector(".sarah").classList.remove("hidden");
+    setTimeout(showVideo, 4000);
+
+}
+
+function showVideo() {
+ document.querySelector(".sarah").classList.add("hidden");
+  document.querySelector(".video-clip").classList.remove("hidden");
+    setTimeout(showJannick, 3000);
+
+}
+
+function showJannick() {
+  
+document.querySelector(".video-clip").classList.add("hidden");
+  document.querySelector(".jannick").classList.remove("hidden");
+
+  
+    setTimeout(fadeOut, 3000);
+
+}
+
+ 
+function fadeOut() {
+  document.querySelector(".jannick").classList.add("fade");
+}
